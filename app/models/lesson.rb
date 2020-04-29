457c3e8cfde9152285cc8880109c55d0068304da
bli013 <btli@ymail.com>
2020-04-29 14:04:43 -0700

    Add presence validations and tests to Lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,9 @@
 class Lesson < ApplicationRecord
+    #Relational belongs_to and has_many.
     belongs_to :code_module
     has_many :questions
+    
+    #Validation below.
+    validates :content, :question, :completed, :code_module_id, :title, presence: true
+
 end
