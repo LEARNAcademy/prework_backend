385a4775901f16f77c78875dcda4872f62f362dc
learn <learn@LEARN-16.local>
2020-04-29 14:08:05 -0700

    Add presence validations to question model. Add rspec tests for question model to verify presence.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
+    #Relational database declarations. 
     belongs_to :lesson
     belongs_to :code_module
+
+    #Presence validations
+    validates :content, :answer, :correct, :completed, :resources, :lesson_id, presence: true 
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
+#Declared global variable for create.
+questioncreate = Question.create
+
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must have content" do
+    expect(questioncreate.errors[:content]).to_not be_empty
+  end
+
+  it "must have an answer" do
+    expect(questioncreate.errors[:answer]).to_not be_empty
+  end
+
+  it "must have the existance of a correct column" do
+    expect(questioncreate.errors[:correct]).to_not be_empty
+  end
+
+  it "must check if its completed" do
+    expect(questioncreate.errors[:completed]).to_not be_empty
+  end
+
+  it "must have a resource" do
+    expect(questioncreate.errors[:resources]).to_not be_empty
+  end
+
+  it "must be attached to a lesson identifier" do
+    expect(questioncreate.errors[:lesson_id]).to_not be_empty
+  end
+  
 end
