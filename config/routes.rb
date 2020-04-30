4b0a9987ab6e5a5a27bd51c34d0bb25d2b819bf1
learn <learn@LEARN-16.local>
2020-04-30 09:45:26 -0700

    Add new Resource model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :resources
   resources :questions
   resources :lessons
   resources :code_modules
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_190318) do
+ActiveRecord::Schema.define(version: 2020_04_30_164233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2020_04_29_190318) do
     t.boolean "completed"
     t.text "resources"
     t.integer "lesson_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.string "name"
+    t.text "link"
+    t.integer "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
