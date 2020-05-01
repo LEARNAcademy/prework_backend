6cdf5b79487e102f8e7a40de6e8f11e7391a00d9
bli013 <btli@ymail.com>
2020-05-01 09:12:13 -0700

    Add methods to questions and also routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/modules' => 'code_modules#index'
+  get '/lessons' => 'lessons#index'
+  get '/questions' => 'questions#index'
 end
