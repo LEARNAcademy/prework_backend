bc2df45bba45d5b5f367d568a0d6300c31e508e8
bli013 <btli@ymail.com>
2020-05-01 16:47:43 -0700

    Add full CRUD to Resource controller

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,20 +2,70 @@ class ResourcesController < ApplicationController
     
     # Method for all Resources
     def index
-        resources = Resource.all
-        render json: resources
+        # Making sure only signed in users can access
+        if user_signed_in?
+            resources = Resource.all
+            render json: resources
+        else
+            render status: 403, plain: 'Please Sign In to Access'
+        end
     end
     # Method for making a new Resource
     def create
-        resource = Resource.create(resource_params)
-        render json: resource
+        # Making sure only signed in users can access
+        if user_signed_in?
+            resource = Resource.create(resource_params)
+            if resource.valid?
+                render json: resource
+            else
+                render json: codeModule.errors, status: :unprocessable_entity
+            end
+        else
+            render status: 403, plain: 'Please Sign In to Access'
+        end
     end
-    # Method to show just 1
+    # Method to show just 1 Resource
     def show
+        # Making sure only signed in users can access
+        if user_signed_in?
+            resource = Resource.find(params[:id])
+            render json: resource
+        else
+            render status: 403, plain: 'Please Sign In to Access'
+        end
+    end
+    # Method to update a Resource
+    def update
+        # Making sure only signed in users can access
+        if user_signed_in?
+            resource = Resource.find(params[:id])
+            resource.update(resource_params)
+            if resource.valid?
+                render json: resource
+            else
+                render json: resource.errors
+            end
+        else
+            render status: 403, plain: 'Please Sign In to Access'
+        end
+    end
+    # Method to delete a Resource
+    def destroy
+        # Making sure only signed in users can access
+        if user_signed_in?
+            resource = Resource.find(params[:id])
+            if resource.destroy
+                render json: resource
+            else
+                render json: resource.errors
+            end
+        else
+            render status: 403, plain: 'Please Sign In to Access'
+        end
     end
     # Strong params below
     private
     def resource_params
-        params.require(:resource).permit(:name, :link)
+        params.require(:resource).permit(:name, :link, :question_id)
     end
 end
