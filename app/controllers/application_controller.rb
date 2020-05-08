6dd6a56c20170a55e970e66db312b8d7e2dd6411
learn <learn@LEARN-16.local>
2020-05-08 15:49:47 -0700

    Add respond_to to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true
+  respond_to :html, :json
+  # protect_from_forgery prepend: true
   skip_before_action :verify_authenticity_token
   # acts_as_token_authentication_handler_for User
     
