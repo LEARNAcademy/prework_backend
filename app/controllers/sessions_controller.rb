a130bf8f83af11a0a6c01696734708114a1b51e1
bli013 <btli@ymail.com>
2020-05-12 12:13:03 -0700

    Add more seeds and added new method to Session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,27 @@
-# class SessionsController < ApplicationController
+class SessionsController < ApplicationController
     
-#     def show
-#         current_user ? head(:ok) : head(:unauthorized)
-#     end
+    def show
+        if current_user ? head(:ok) : head(:unauthorized)
+    end
 
-#     def create
-#         user = User.where(email: params[:email]).first
-#         if user&.valid_password?(params[:password])
-#             render json: user.as_json(only: [:email, :authentication_token]), status: :created
-#         else 
-#             head(:unauthorized)
-#         end
-#     end
+    def create
+        user = User.where(email: params[:email]).first
+        if user&.valid_password?(params[:password])
+            render json: user.as_json(only: [:email, :authentication_token]), status: :created
+        else 
+            head(:unauthorized)
+        end
+    end
 
-#     def destroy
+    def new
+        if user&.valid_password?(params[:password])
+            render json: user.as_json(only: [:email, :authentication_token]), status: :created
+        else 
+            head(:unauthorized)
+        end
+    end
 
-#     end
-# end
+    def destroy
+
+    end
+end
